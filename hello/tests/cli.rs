688e60b8e7ee00cdec076007d79acbfca8b0aa10
Jonas Ångström <jonas.aangstroem@gmail.com>
2025-11-25 21:14:17 +0100

    added test to test stout

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -1,4 +1,5 @@
 use assert_cmd::cargo;
+use pretty_assertions::assert_eq;
 
 #[test]
 fn works() {
@@ -9,4 +10,8 @@ fn works() {
 fn runs() {
     let mut cmd = cargo::cargo_bin_cmd!("hello");
     cmd.assert().success();
+    let output = cmd.output().expect("fail");
+    let stdout = String::from_utf8(output.stdout).expect("fail");
+    assert_eq!(stdout, "Hello, world!\n");
 }
+
