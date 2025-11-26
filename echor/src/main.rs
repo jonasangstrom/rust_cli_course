a73aa9c807abf5e89c1b1c518c000cf438cf0c00
Jonas Ångström <jonas.aangstroem@gmail.com>
2025-11-26 07:03:28 +0100

    Working version of echor

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,11 +1,29 @@
-use clap::Command;
+use clap::{Command, Arg, ArgAction};
 
 fn main() {
-    let _matches = Command::new("echor")
+    let matches = Command::new("echor")
         .version("0.1.0")
         .author("Jonas Ångström")
         .about("Rust version of echo")
+        .arg(
+            Arg::new("text")
+            .value_name("TEXT")
+            .required(true)
+            .num_args(1..)
+        )
+        .arg(
+            Arg::new("omit_newline")
+            .short('n')
+            .action(ArgAction::SetTrue)
+            .help("Do not print newline.")
+        )
         .get_matches();
 
-    println!("{:?}", _matches);
+    let omit_newline = matches.get_flag("omit_newline");
+    let ending: &str = if omit_newline {""} else {"\n"};
+
+    let text: Vec<String> = matches.get_many("text").unwrap().cloned().collect();
+
+    print!("{}{ending}", text.join(" "));
 }
+
