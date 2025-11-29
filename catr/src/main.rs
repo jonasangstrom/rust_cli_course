2b65bf74cc2da1348dfd83e079ae7e59ee6864e8
Jonas Ångström <jonas.aangstroem@gmail.com>
2025-11-29 23:07:16 +0100

    Added parsers in catr

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:#?}", args);
+}
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// "Rust version of cat"
+struct Args {
+    #[arg(required(true))]
+    files: Vec<String>,
+    #[arg(short('n'))]
+    number: bool,
+    #[arg(short('b'))]
+    #[arg(long("number-nonblank"))]
+    number_nonblank: bool,
 }
