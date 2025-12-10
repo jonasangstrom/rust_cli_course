d0100c79bd36aea0d74e12b9a7c41cab47ee3331
Jonas Ångström <jonas.aangstroem@gmail.com>
2025-12-10 17:34:43 +0100

    lines and linebreaks are now correct.

--- headr/src/main.rs
+++ headr/src/main.rs
@@ -4,26 +4,38 @@ use std::io::{self, BufRead, BufReader};
 use anyhow::Result;
 
 fn main() {
+    run().unwrap();
+}
+
+fn run() -> Result<()> {
     let args = Args::parse();
-    let number_of_lines = args.lines;
+    let number_of_lines = args.lines as usize;
     let filenames = args.files;
+    let n_files = filenames.len();
     let print_filename = match filenames.len()
     {
         1 => false,
         _ => true,
     };
-    for filename in filenames {
+    for (n_file, filename) in filenames.iter().enumerate(){
         if print_filename
         {
-            println!("==> {filename} <==\n");
+            print!("==> {filename} <==\n");
         }
         let buffer = open(&filename);
         match buffer {
             Err(err) => eprintln!("Failed to read file {err}"),
-            Ok(file) => print_to_stout( file, number_of_lines as usize),
+            Ok(file) => print_file_to_stout_lines( file, &number_of_lines)?,
+        }
+        let last_file = (n_file + 1) != n_files;
+        if print_filename & last_file
+        {
+            print!("\n");
         }
     }
+    Ok(())
 }
+
 
 fn open(filename: &str) -> Result<Box<dyn BufRead>> {
     match filename {
@@ -32,17 +44,17 @@ fn open(filename: &str) -> Result<Box<dyn BufRead>> {
     }
 }
 
-fn print_to_stout(buffer_box: Box<dyn BufRead>, number_of_lines: usize) {
-
-    for (line_number, line_result)  in buffer_box.lines().enumerate() {
-        match line_result {
-            Err(err) => eprintln!("Failed to read line {err}"),
-            Ok(line) => println!("{line}"),
-        }
-        if (line_number + 1) >= number_of_lines {
+fn print_file_to_stout_lines(mut file: Box<dyn BufRead>, number_of_lines: &usize) -> Result<()> {
+    let mut line =  String::new();
+    for _line_number in 0..*number_of_lines {
+        let bytes = file.read_line(&mut line)?;
+        if bytes == 0 {
             break;
         }
+        print!("{line}");
+        line.clear();
     }
+    Ok(())
 }
 
 #[derive(Debug, Parser)]
