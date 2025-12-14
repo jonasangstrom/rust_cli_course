515888683d6558ebff5b4a12164851166d23d12c
Jonas Ångström <jonas.aangstroem@gmail.com>
2025-12-14 21:33:42 +0100

    Added helps

--- wcr/src/main.rs
+++ wcr/src/main.rs
@@ -6,19 +6,28 @@ fn main() {
 
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
+/// Rust version of wc
 struct Args {
+    /// Input file(s)
     #[arg(
         value_name="FILE",
-        help="Input file(s)",
         default_value="-"
     ) ]
     files: Vec<String>,
+
+    /// Show line count
     #[arg(short, long)]
     lines: bool,
+
+    /// Show word count
     #[arg(short, long)]
     words: bool,
+
+    /// Show bytes count
     #[arg(short('c'), long)]
     bytes: bool,
+
+    /// Show chars count
     #[arg(short('m'), long, conflicts_with("bytes"))]
     chars: bool
 }
