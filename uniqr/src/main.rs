1f2a58b3a173b55a97f6ddf10452c3d8039ae52c
Jonas Ångström <jonas.aangstroem@gmail.com>
2025-12-20 08:25:40 +0100

    Added parsing

--- uniqr/src/main.rs
+++ uniqr/src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{args:?}");
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// Input file
+    #[arg(value_name = "IN_FILE", default_value = "-")]
+    in_file: String,
+
+    /// Output file
+    #[arg(value_name = "OUT_FILE")]
+    out_file: Option<String>,
+
+    /// Show counts
+    #[arg(short, long)]
+    count: bool,
 }
