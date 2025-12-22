7ebbdb38505f16bb99eecb0748ba7eaf986ae28d
Jonas Ångström <jonas.aangstroem@gmail.com>
2025-12-22 22:45:38 +0100

    Works for some now. Linebreak may be problem.

--- uniqr/src/main.rs
+++ uniqr/src/main.rs
@@ -20,28 +20,52 @@ fn run(args: Args) -> Result<()> {
             return Err(err);
         }
     };
-    read_file(buffer)?;
+    read_file(buffer, args.count)?;
     Ok(())
 }
 
-fn read_file(mut buffer: Box<dyn BufRead>) -> Result<()> {
+fn read_file(mut buffer: Box<dyn BufRead>, print_line_number: bool) -> Result<()> {
     let mut line = String::new();
     let mut old_line = String::new();
+    let mut first = true;
+    let mut optional_line_number: Option<i32> = match print_line_number {
+        true => Some(0),
+        false => None,
+    };
     loop {
+        optional_line_number = match optional_line_number {
+            Some(line_number) => Some(line_number + 1),
+            None => None,
+        };
         let bytes = buffer.read_line(&mut line)?;
         if bytes == 0 {
+            write_output(&old_line, &optional_line_number);
             break;
         }
-        if old_line == line {
-            line.clear();
-            continue;
+
+        if old_line != line {
+            if first {
+                first = false;
+            } else {
+                write_output(&old_line, &optional_line_number);
+            }
+            old_line = format!("{line}");
+            optional_line_number = match optional_line_number {
+                Some(_) => Some(0),
+                None => None,
+            };
+        } else {
         }
-        print!("{line}");
-        old_line.clear();
-        old_line = format!("{line}");
         line.clear();
     }
     Ok(())
+}
+
+fn write_output(line: &String, optional_line_number: &Option<i32>) {
+    match optional_line_number {
+        Some(line_number) => print!("{line_number:>4} {line}"),
+        None => print!("{line}"),
+    };
 }
 
 #[derive(Parser, Debug)]
