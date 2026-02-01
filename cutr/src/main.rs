d14f6e7db27e484750c87a8bcd92a06675fc6796
Jonas Ångström <jonas.aangstroem@gmail.com>
2026-02-01 08:29:37 +0100

    Added parser

--- cutr/src/main.rs
+++ cutr/src/main.rs
@@ -1,3 +1,33 @@
+use clap::Parser;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+/// Rust version of 'cut'
+struct Args {
+    /// Input file(s)
+    #[arg(default_value = "-")]
+    files: Vec<String>,
+    /// Field delimiter
+    #[arg(short, long, value_name = "DELIMITER", default_value = "\t")]
+    delimiter: String,
+    /// Selected fields
+    #[command(flatten)]
+    extract: ArgsExtract,
+}
+#[derive(Debug, clap::Args)]
+#[group(required = true, multiple = false)]
+struct ArgsExtract {
+    ///Selected fields
+    #[arg(short, long, value_name = "FIELDS")]
+    fields: Option<String>,
+    ///Selected bytes
+    #[arg(short, long, value_name = "BYTES")]
+    bytes: Option<String>,
+    ///Selected chars
+    #[arg(short, long, value_name = "CHARS")]
+    chars: Option<String>,
 }
